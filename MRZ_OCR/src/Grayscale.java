766da29585f21a2dd65b0e4d9a017e0607654229
richardpesch <richard.pesch@capgemini.com>
2017-04-24 09:34:27 +0200

    GrayScale

---
+++ MRZ_OCR/src/Grayscale.java
@@ -0,0 +1,62 @@
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+import org.apache.xmlgraphics.ps.ImageFormGenerator;
+
+import com.lowagie.text.Image;
+
+public class Grayscale {
+	
+	BufferedImage grayImage;
+	BufferedImage testImage;
+	
+	public Grayscale(){
+		File imageFile = new File("D:\\Users\\RPESCH\\Downloads\\TEST.jpg");
+		try{
+		BufferedImage image = ImageIO.read(imageFile);
+		//BufferedImage bwImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+		//Graphics2D graphics = bwImage.createGraphics();
+	    //graphics.drawImage(image, 0, 0, null);
+		
+		//grayImage = getScaledImage(bwImage, bwImage.getWidth()+100, bwImage.getHeight()+100);
+	    testImage = getScaledImage(image, image.getWidth()+400, image.getHeight()+400);
+		grayImage = toGray(testImage);
+		}catch(IOException e){
+			System.out.println(e);
+		}
+	}
+	public BufferedImage toGray(BufferedImage image){
+		int width = image.getWidth();
+		int height = image.getHeight();
+		for(int i = 0; i<height; i++){
+			for(int j = 0; j<width; j++){
+				Color c = new Color(image.getRGB(j, i));
+				int red = (int)(c.getRed()*0.11);
+				int green = (int)(c.getGreen()*0.12);
+				int blue = (int)(c.getBlue()*0.47);
+				int sum = red+green+blue;
+				Color newColor = new Color(sum,sum,sum);
+				image.setRGB(j, i, newColor.getRGB());
+			}
+		}
+		return image;
+	}
+	
+	public BufferedImage getScaledImage(BufferedImage image, int width, int height){
+		int imageWidth = image.getWidth();
+		int imageHeight = image.getHeight();
+		
+		double scaleX = (double)width/imageWidth;
+		double scaleY = (double)height/imageHeight;
+		AffineTransform scaleTransform = AffineTransform.getScaleInstance(scaleX, scaleY);
+		AffineTransformOp bilinearScaleOp = new AffineTransformOp(scaleTransform, AffineTransformOp.TYPE_BILINEAR);
+		
+		return bilinearScaleOp.filter(image, new BufferedImage(width, height, image.getType()));
+	}
+}
--- MRZ_OCR/src/Main.java
+++ MRZ_OCR/src/Main.java
@@ -1,20 +1,26 @@
 
 import java.io.*;
+
+import javax.imageio.ImageIO;
+
 import net.sourceforge.tess4j.*;
 
 public class Main {
 	public static void main(String[] args) {
-		File imageFile = new File("D:\\Users\\TEBLOM\\Downloads\\TEST.jpg");
 		Tesseract instance = Tesseract.getInstance(); //
-
+		Grayscale grayScale = new Grayscale();
+		File test = new File("D:\\Users\\RPESCH\\Documents\\GitHub\\ID\\Tess4J\\Output4.jpg");
+		
 		try {
-			
-			instance.setDatapath("D:\\Users\\TEBLOM\\Documents\\GitHub\\ID\\Tess4J");
-			String result = instance.doOCR(imageFile);
+			instance.setDatapath("D:\\Users\\RPESCH\\Documents\\GitHub\\ID\\Tess4J");
+			ImageIO.write(grayScale.grayImage, "jpg", test);
+			String result = instance.doOCR(grayScale.grayImage);
 			System.out.println(result);
 
 		} catch (TesseractException e) {
 			System.err.println(e.getMessage());
+		}catch (IOException ex){
+			System.out.println(ex);
 		}
 	}
 }
